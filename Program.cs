e35259dfb37511e0af938bcab24d2566d9b1b1c4
Tomasz Szumski <Tomasz.Szumski@MyGeorgian.ca>
2020-10-07 02:03:30 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Converter.Models;
+using System;
+using System.Linq;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string unicodeString = "Tomasz Szumski";
+            int[] fullNameBytes = new[] { 1, 1, 2, 3, 5, 8, 13 }; //Fibonacci Sequence
+            string cipherasString = String.Join(",", fullNameBytes.Select(x => x.ToString())); //FOr display
+
+            int encryptionDepth = 20;
+
+            Encrypter encrypter = new Encrypter(unicodeString, fullNameBytes, encryptionDepth);
+
+            //Deep Encrytion
+            string nameDeepEncryptWithCipher = Encrypter.DeepEncryptWithCipher(unicodeString, fullNameBytes, encryptionDepth);
+            Console.WriteLine($"Deep Encrypted {encryptionDepth} times using the cipher {{{cipherasString}}} {nameDeepEncryptWithCipher}");
+
+            string nameDeepDecryptWithCipher = Encrypter.DeepDecryptWithCipher(nameDeepEncryptWithCipher, fullNameBytes, encryptionDepth);
+            Console.WriteLine($"Deep Decrypted {encryptionDepth} times using the cipher {{{cipherasString}}} {nameDeepDecryptWithCipher}");
+
+           
+        }
+    }
+}
